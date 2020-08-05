ab93431a417f2e032f9007ce2f84bb44d20a693d
Tony Hung <tbass134@gmail.com>
2020-08-05 16:51:18 -0400

    ere

--- app/app.py
+++ app/app.py
@@ -88,6 +88,3 @@ def upload(filename):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-        
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
